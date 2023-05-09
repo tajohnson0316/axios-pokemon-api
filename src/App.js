303c2a11739e787970264dd27c0ca4761e38f543
tajohnson0316 <armanijohnson1331@gmail.com>
2023-05-09 13:27:59 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const getPokemon = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((serverResponse) => {
+        console.log(serverResponse.data.results);
+        const pokemonNames = serverResponse.data.results.map(
+          (pokemon) => pokemon.name
+        );
+        setPokemonList(pokemonNames);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <fieldset>
+      <div className="container">
+        <legend>App.jsx</legend>
+        <div className="d-flex justify-content-center mb-3">
+          <button className="btn btn-warning" onClick={getPokemon}>
+            Fetch All Pokemon
+          </button>
+        </div>
+        <div className="text-center">
+          {pokemonList.map((pokemon, idx) => {
+            pokemon = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);
+            return <p key={idx}>{pokemon}</p>;
+          })}
+        </div>
+      </div>
+    </fieldset>
   );
 }
 
